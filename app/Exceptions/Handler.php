91328605d6f75157a7541cef83b1a9c740235c1a
erg <erg@gtr.tr>
2025-04-28 01:32:49 +0700

    fix 1

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -51,10 +51,8 @@ class Handler extends ExceptionHandler
 
     public function render($request, Throwable $exception)
     {
-        if ((env('APP_ENV') == 'production') && !str_contains($request->fullUrl(), '/admin')) {
-            return redirect()->route('landingPage');
-        }
+       return parent::render($request, $exception);
 
-        return parent::render($request, $exception);
+        
     }
 }
